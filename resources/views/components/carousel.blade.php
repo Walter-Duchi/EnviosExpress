9336d5a3ced03c8839b321a8818790a6ca510d4e
Walter-Duchi <waltduchi@gmail.com>
2025-01-02 17:54:29 -0500

    add empty components from home & app layout

---
+++ resources/views/components/carousel.blade.php
@@ -0,0 +1,11 @@
+<div class="slider">
+    <div class="slides">
+        <div class="slide"><img src="assets/img/camera.png" alt="itemSlide"></div>
+        <div class="slide"><img src="assets/img/headphone.png" alt="itemSSlide"></div>
+        <div class="slide"><img src="assets/img/electronic.png" alt="itemSlide"></div>
+        <div class="slide"><img src="assets/img/phone.png" alt="itemSlide"></div>
+    </div>
+    <!-- Botones para mover de promociones -->
+    <button class="prev" onclick="moverSlide(-1)">&#10094;</button>
+    <button class="sgn" onclick="moverSlide(1)">&#10095;</button>
+</div>
